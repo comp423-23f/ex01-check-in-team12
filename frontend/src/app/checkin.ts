d98d9f51e759c566e8aeb7dcb76c5517b2b89b00
sarah <sz1204@ad.unc.edu>
2023-09-08 15:35:41 +0000

    finished story e of displaying checked in members, and also having registered and checked in members show up by alphabet and time

---
+++ frontend/src/app/checkin.ts
@@ -0,0 +1,8 @@
+export class checkin {
+    constructor(
+        public first: string,
+        public last: string,
+        public pid: string,
+        public date: Date, // date and time
+    ) {}
+}
